f097edaacb4d9bbf640cf54c2240638b895aae3f
efemayoglu <efemayoglu@gmail.com>
2021-06-25 21:50:19 +0300

    added urlMapService with byUserId

--- src/test/java/tapu/urlshortener/business/UrlMapServiceTest.java
+++ src/test/java/tapu/urlshortener/business/UrlMapServiceTest.java
@@ -1,2 +1,31 @@
-package tapu.urlshortener.business;public class UrlMapServiceTest {
+package tapu.urlshortener.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tapu.urlshortener.business.abstracts.UrlMapService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UrlMapServiceTest {
+
+    private UrlMapService mapService;
+
+    @Autowired
+    public UrlMapServiceTest(UrlMapService mapService){
+        this.mapService = mapService;
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void shouldFindUrlsByUserId(){
+        var result = mapService.getByCreatedUser(1);
+
+       assertTrue(result.getData().size() > 0);
+    }
+
 }
