5e5ece4e5c6b16b9c2916771dd3225f7945f096e
efemayoglu <efemayoglu@gmail.com>
2021-06-25 22:37:32 +0300

    added logintests

---
+++ src/test/java/tapu/urlshortener/businessTests/LoginTests.java
@@ -0,0 +1,42 @@
+package tapu.urlshortener.businessTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tapu.urlshortener.businessTests.abstracts.LoginService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class LoginTests {
+
+    private LoginService loginService;
+
+    @Autowired
+    public LoginTests(LoginService loginService){
+        this.loginService = loginService;
+    }
+
+    @Test
+    public void shouldLoginUser(){
+        final String username = "efemayoglu";
+        final String password = "123456";
+
+        var result = loginService.getByUsernameAndPassword(username, password);
+
+        assertNotNull(result.getData());
+        assertNotNull(result.getData().getId());
+        assertTrue(result.getData().getId() != 0);
+        assertTrue(result.getData().getId() > 0);
+    }
+
+    @Test
+    public void shouldNotLoginUser(){
+        final String username = "efemayoglu";
+        final String password = "1234561";
+
+        var result = loginService.getByUsernameAndPassword(username, password);
+
+        assertNull(result.getData());
+    }
+}
